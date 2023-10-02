c138e55f16f3c67647c07612615ff05848a35f32
maceteligolden <maceteligolden@gmail.com>
2023-10-02 21:34:43 +0100

    feat:add admin auth router

--- src/modules/Auth/routes/admin.route.ts
+++ src/modules/Auth/routes/admin.route.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { container } from 'tsyringe';
+import { AdminauthController } from "../controller";
 
 const adminAuthRouter = Router();
-// const controller = container.resolve(AdminauthController);
+const controller = container.resolve(AdminauthController);
 
-// adminAuthRouter.post('/login', (req: Request, res: Response, next: NextFunction)=>controller.login(req, res, next));
+adminAuthRouter.post('/login', (req: Request, res: Response, next: NextFunction)=>controller.login(req, res, next));
+adminAuthRouter.post('/register', (req: Request, res: Response, next: NextFunction) => controller.register(req, res, next));
+adminAuthRouter.post('/forgotpassword', (req: Request, res: Response, next: NextFunction) => controller.forgotPassword(req, res, next));
+adminAuthRouter.patch('/changepassword', (req: Request, res: Response, next: NextFunction)=>controller.changePassword(req, res, next));
+adminAuthRouter.post('/verify-otp', (req: Request, res: Response, next: NextFunction)=>controller.verifyOtp(req, res, next));
 
 export default adminAuthRouter;
